9647147a2f7b6fa80f5992af5f0c1b09ed1e3572
dhjasnani <asnani.dheeraj@yahoo.com>
2016-06-30 00:01:22 +0530

    Presence of SD card detection implemented

--- app/src/main/java/son_gohan/fadduplayer/ActivityDisplaySngs.java
+++ app/src/main/java/son_gohan/fadduplayer/ActivityDisplaySngs.java
@@ -1,13 +1,49 @@
 package son_gohan.fadduplayer;
 
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.Button;
 
-public class ActivityDisplaySngs extends AppCompatActivity {
+import java.net.URI;
+import java.util.ArrayList;
 
+public class ActivityDisplaySngs extends AppCompatActivity implements View.OnClickListener {
+
+    Button importButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void init()
+    {
+        importButton = (Button) findViewById(R.id.import_btn);
+        importButton.setOnClickListener(ActivityDisplaySngs.this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    private ArrayList<Song> listAllSongs(){
+        Cursor cursor;
+        ArrayList<Song> arrayList = new ArrayList<Song>();
+        Uri allSongUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        String selection = "";
+        if()
+        return arrayList;
+    }
+    private static boolean isSdCardPresent()
+    {
+        return android.os.Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
     }
 }
---
+++ app/src/main/java/son_gohan/fadduplayer/Song.java
@@ -0,0 +1,7 @@
+package son_gohan.fadduplayer;
+
+/**
+ * Created by Freeware Sys on 29-Jun-16.
+ */
+public class Song {
+}
