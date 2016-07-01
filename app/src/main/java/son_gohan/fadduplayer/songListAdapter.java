f9f0011abb376d2cae1ba29ad21220cd0187d9e1
dhjasnani <asnani.dheeraj@yahoo.com>
2016-07-02 01:11:41 +0530

    ListView Adapter added

--- app/src/main/java/son_gohan/fadduplayer/songListAdapter.java
+++ app/src/main/java/son_gohan/fadduplayer/songListAdapter.java
@@ -1,30 +1,46 @@
 package son_gohan.fadduplayer;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Created by Dheeraj Asnani on 02-Jul-16.
  */
 public class songListAdapter  extends BaseAdapter{
+    private Context mcontext;
+    private ArrayList<Song> SongList;
+
+    public songListAdapter(Context context, ArrayList<Song> arrayList)
+    {
+        mcontext = context;
+        this.SongList = arrayList;
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return SongList.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+
+
+        return SongList.get(position);
     }
 
     @Override
     public long getItemId(int position) {
+
         return 0;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+
         return null;
     }
 }
