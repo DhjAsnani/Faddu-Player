f04ac2f1e7de02a3ee11998a78e53bb3348aebba
dhjasnani <asnani.dheeraj@yahoo.com>
2016-07-02 01:42:28 +0530

    Service class added

--- app/src/main/java/son_gohan/fadduplayer/MusicService.java
+++ app/src/main/java/son_gohan/fadduplayer/MusicService.java
@@ -1,7 +1,33 @@
 package son_gohan.fadduplayer;
 
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
 /**
- * Created by Freeware Sys on 02-Jul-16.
+ * Created by Dheeraj Asnani on 02-Jul-16.
  */
-public class MusicService {
+public class MusicService extends Service implements MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener, MediaPlayer.OnCompletionListener {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        return false;
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+
+    }
 }
